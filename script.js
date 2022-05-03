e59ec0d713f0deb5db8472203e96205ac440d5d2
Alex <alexghioca27@gmail.com>
2022-05-03 21:01:36 +0300

    progress

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const container = document.querySelector('#container');
+
+let rows = 16;
+let columns = 16;
+
+function drawSquare(columns, rows) {
+
+    let square = document.createElement('div');
+    square.classList = 'grid-square';
+    container.appendChild('square');
+    square.setAttribute('style', 'height: 100px; width: 100px; outline: thin solid')
+                
+    
+}
+
+function drawGrid(columns, rows) {
+    for (let i = 0; i < columns; i++) {
+        for (let i = 0; i < rows; i++) {
+            drawSquare(columns, rows);
+        }
+    }
+}
+
+drawGrid(columns, rows);
+
+
+}
